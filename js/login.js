785b4f7137f018fa790bb39725c2d73f9c894cc3
onyiii <onyinyechukwuma14@gmail.com>
2019-09-17 18:05:59 +0200

    created a new html for payroll list

--- js/login.js
+++ js/login.js
@@ -7,5 +7,33 @@ $(document).ready(function() {
       const username = $('#username').val();
       const password = $('#password').val();
       const email = $('#email').val(); 
-    })
-})
+      console.log (fullname, username,password,email);
+      if (!fullname || !username || !password || !email) {
+        $('.writeup').html('Kindly fill in all fields');
+        return;
+      }
+      
+    $.ajax({
+        method: 'GET',
+        url: `http://localhost:3000/users?email=${email}`,
+        data: {
+          email,
+        },
+        
+        success: function(response) {
+          if (response.length) {
+            $('.writeup').html('User already exist');
+          } else {
+            $.ajax({
+              method: 'POST',
+              url: 'http://localhost:3000/users',
+              data: {
+                fullname,
+                username,
+                email,
+                password,
+              },
+    
+              success: function() {
+              $('.writeup').html('Registered');}
+});
