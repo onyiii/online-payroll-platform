5e32d609780f8188eb206605e3d31b3ab81115ee
onyiii <onyinyechukwuma14@gmail.com>
2019-09-17 17:14:02 +0200

    writing javascript function

--- js/login.js
+++ js/login.js
@@ -1,1 +1,11 @@
-$()
+$('.message a').click(function(){$('Form').animate({height: "toggle",opacity: "toggle"}, "slow");});
+$(document).ready(function() {
+    
+    $('.create').click(function(event) {
+      event.preventDefault();
+      const fullname = $('#fullname').val();
+      const username = $('#username').val();
+      const password = $('#password').val();
+      const email = $('#email').val(); 
+    })
+})
