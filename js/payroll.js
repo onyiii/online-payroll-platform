dd4d9fe70711da98c8765ea0431abdee5a452ddb
onyiii <onyinyechukwuma14@gmail.com>
2019-09-18 22:16:52 +0200

    changed the table content

--- js/payroll.js
+++ js/payroll.js
@@ -1,1 +1,11 @@
 function addHtmlTableRow() {
+    let table = document.getElementById('table'),
+    newRow = table.insertRow(table.length),
+    cell1 = newRow.insertRowCell(0),
+    cell2 = newRow.insertRowCell(1),
+    cell3 = newRow.insertRowCell(2),
+    cell4 = newRow.insertRowCell(3),
+    cell5 = newRow.insertRowCell(5),;
+}
+// table
+$
