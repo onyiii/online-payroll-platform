97539aa3cb9a2f7d4a33fdd68ecd2b5b9a0a3017
onyiii <onyinyechukwuma14@gmail.com>
2019-09-19 15:14:44 +0200

    added ajax.post to get

--- js/payroll.js
+++ js/payroll.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("#myInput").on("keyup", function() {
+    $("#myInput").keyup (function() {
       const value = $(this).val().toLowerCase();
       $("#myTable tr").filter(function() {
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
@@ -15,25 +15,44 @@ $(document).ready(function(){
     if (!name || !position || !startDate || !salary) {
         $('.regMessage').html('Kindly fill in all fields');
         return;
-      }
- 
-    $(".data-table tbody").append("<tr data-name='"+name+"' data-position='"+position+"'data-startDate='"+startDate+"'data-salary='"+salary+"'><td>"+name+"</td><td>"+position+"</td><td>"+startDate+"</td><td>"+salary+"</td><td><button class='btn btn-info btn-xs btn-edit'>Edit</button><button class='btn btn-danger btn-xs btn-delete'>Delete</button></td></tr>");
+     }  else  {
+       $.ajax(
+        {
+          method:'POST',
+          url:"http://localhost:3000/records",
+          data: {
+            name,
+            position,
+            startDate,
+            salary
+          }
+        }
+       ) 
+     }   
+    
+    
 
     $('#name').val('');
     $('#position').val('');
     $('#startDate').val('');
     $('#salary').val('');
 });
+$.get("http://localhost:3000/records",function(data, status){
+      data.forEach(record=>{ 
+        $("#myTable").append(`<tr ><td value ="${record.name}">${record.name}</td><td value ="${record.position}">"${record.position}"</td><td value ="${record.startDate}">${record.startDate}</td><td value ="${record.salary}">${record.salary}</td><td><button class='btn btn-info btn-xs btn-edit'>Edit</button><button class='btn btn-danger btn-xs btn-delete'>Delete</button></td></tr>`);}) 
+  } 
+
+    )
 
 $("body").on("click", ".btn-delete", function(){
     $(this).parents("tr").remove();
 });
 
 $("body").on("click", ".btn-edit", function(){
-    const name = $(this).parents("tr").attr('data-name');
-    const position = $(this).parents("tr").attr('data-position');
-    const startDate= $(this).parents("tr").attr('data-startDate');
-    const salary = $(this).parents("tr").attr('data-salary');
+    const name = $(this).parents("tr").attr('value');
+    const position = $(this).parents("tr").attr('value');
+    const startDate= $(this).parents("tr").attr('value');
+    const salary = $(this).parents("tr").attr('value');
 
     $(this).parents("tr").find("td:eq(0)").html('<input name="edit_name" value="'+name+'">');
     $(this).parents("tr").find("td:eq(1)").html('<input name="edit_position" value="'+position+'">');
